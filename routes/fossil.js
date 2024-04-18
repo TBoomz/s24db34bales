86ad1bcd43265b497cced78ec81520a68e168baa
TBoomz <111537214+TBoomz@users.noreply.github.com>
2024-04-17 22:42:51 -0500

    ahh?

--- routes/fossil.js
+++ routes/fossil.js
@@ -12,7 +12,7 @@ const secured = (req,res,next)=>{
 /* GET home page. */
 router.get('/', fossil_controller.fossil_view_all_Page);
 router.get('/detail', fossil_controller.fossil_view_one_Page);
-router.get('/create', secured, fossil_controller.pizza_create_Page);
+router.get('/create', secured, fossil_controller.fossil_create_Page);
 router.get('/update', secured, fossil_controller.fossil_update_Page);
 router.get('/delete', secured, fossil_controller.fossil_delete_Page);
 
