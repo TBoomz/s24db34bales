a4e351d955673f318022cdb41f49d2fee1b1267a
TBoomz <111537214+TBoomz@users.noreply.github.com>
2024-04-17 22:34:21 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,5 @@
 var createError = require('http-errors');
 var express = require('express');
-var app = express();
-var Fossil = require("./models/fossil");
-app.get('./models/fossil', Fossil.FossilRouter);
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
