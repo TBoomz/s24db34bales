645c6aa91584076dcc1d0e2da7a01f19336c4117
TBoomz <111537214+TBoomz@users.noreply.github.com>
2024-04-17 20:22:50 -0500

    AHH

--- routes/fossil.js
+++ routes/fossil.js
@@ -1,10 +1,20 @@
 var express = require('express');
+const pizza_controller = require('../controllers/fossils');
 var router = express.Router();
 
-/* GET fossils page. */
-router.get('/', function(req, res, next) {
-  res.render('fossils', { title: 'Search Results - Fossils' });
-});
+const secured = (req,res,next)=>{
+    if(req.user){
+        return next();
+    }
+    res.redirect("/login");
+}
+
+/* GET home page. */
+router.get('/', fossil_controller.fossil_view_all_Page);
+router.get('/detail', fossil_controller.fossil_view_one_Page);
+router.get('/create', secured, fossil_controller.pizza_create_Page);
+router.get('/update', secured, fossil_controller.fossil_update_Page);
+router.get('/delete', secured, fossil_controller.fossil_delete_Page);
+
 
 module.exports = router;
-
